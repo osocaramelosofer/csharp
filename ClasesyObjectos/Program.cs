0663ace50a474f630e3623dd6423bd22aa5cc13a
ZJ\F4533561 <A95F4533561@saint-gobain.com>
2023-02-06 12:20:03 -0600

    Clases y objectos

---
+++ ClasesyObjectos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesyObjectos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale sale1 = new Sale();
+            Console.WriteLine(sale1.GetInfo());
+            Console.WriteLine(sale1.Show());
+        }
+
+        public class Sale
+        {
+            int total;
+            DateTime date;
+
+            public string GetInfo()
+            {
+                return total + ' ' + date.ToLongDateString();
+            }
+
+            public string Show()
+            {
+                return "hola soy una ventana";
+            }
+        }
+    }
+}
